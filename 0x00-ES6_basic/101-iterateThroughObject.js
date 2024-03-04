c28061dd0149173acab17020393e595a3b0fd6c1
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-04 15:11:55 +0000

    bob

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let longString = ``; 
   for (let i = 0; i < reportWithIterator.length; i++) {
-    if (i === reportWithIterator.length -1) {
+    if (i === reportWithIterator.length - 1) {
       longString += `${reportWithIterator[i]}`;
     } else {
       longString += `${reportWithIterator[i]} | `;
