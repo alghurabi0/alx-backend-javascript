1b5c7ced50e5e7e797410f766ad2471f9212019f
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-04 14:54:22 +0000

    all

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const arrayOfArr = Object.values(report.allEmployees);
+  let names = []
+  for (const arr of arrayOfArr) {
+    for (const name of arr) {
+      names.push(name);
+    }
+  }
+  return names;
+}
