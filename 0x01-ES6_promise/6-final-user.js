495b8530db4ced5685f7ad218a51fa0e9c7376f4
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-05 21:40:01 +0000

    fjdks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
-import { signUpUser } from "./4-user-promise.js";
-import { uploadPhoto } from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled[
-    (signUpUser(firstName, lastName), uploadPhoto(fileName))
-  ].then((values) =>
-    valuse.map((v) => ({
-      status: v.status,
-      value: v.status === "fulfilled" ? v.value : String(v.reason),
-    }))
-  );
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => (
+      values.map((v) => ({
+        status: v.status,
+        value: v.status === 'fulfilled' ? v.value : String(v.reason),
+      }))
+    ));
 }
