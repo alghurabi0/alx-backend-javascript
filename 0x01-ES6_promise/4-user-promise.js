a7cf50bdd84d9b68ecdc8f3969a301d0eee365d6
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2024-03-05 16:24:52 +0200

    task 4

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    if (!firstName || !lastName) {
+      reject("First name and last name are required");
+    } else {
+      resolve({ firstName, lastName });
+    }
+  });
+}
