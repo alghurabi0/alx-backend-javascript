31b16c22dd04c1b61eba49c5535218178197f399
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-07 14:35:48 +0000

    dkfjs

---
+++ 0x02-ES6_classes/4-main.js
@@ -0,0 +1,8 @@
+import Pricing from './4-pricing.js';
+import Currency from './3-currency.js';
+
+const c = new Currency("EUR", "Euro")
+console.log(c);
+const p = new Pricing(100, c)
+console.log(p);
+console.log(p.displayFullPrice());
