2ae750a2a7eb8b9e9b2332b63b0e91de49c10b1c
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-04-30 19:31:53 -0400

    http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,10 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running and listening on port ${PORT}`);
-});
+app.listen(PORT);
 
 module.exports = app;
